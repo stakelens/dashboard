db333ecbf7beede2a34578695727d4a1979309a5
crisog <ortega.cpp@gmail.com>
2024-04-19 00:58:30 -0400

    feat: fonts + layouts + home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,26 @@
+import node from '@astrojs/node';
+import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  server: {
+    host: '0.0.0.0'
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    react()
+  ],
+  image: {
+    remotePatterns: [{ protocol: 'https' }],
+    domains: ['r2.dev']
+  }
+});

dada018c16156bc191895a5f60501cafa91c3dd7
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-07-01 10:33:57 -0400

    charts filtering by days work

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -9,15 +9,54 @@ import {
   Area
 } from 'recharts';
 
+const DAY = 1000 * 60 * 60 * 24;
+const WEEK = 7 * DAY;
+const MONTH = 30 * DAY;
+const YEAR = 12 * MONTH;
+
+function FilterOption({
+  filter,
+  setFilter,
+  value,
+  label
+}: {
+  filter: number;
+  setFilter: (value: number) => void;
+  value: number;
+  label: string;
+}) {
+  return (
+    <div
+      className={
+        filter === value
+          ? 'bg-[#2E46C8] rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer'
+          : 'bg-[#242424] rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer'
+      }
+      onClick={() => setFilter(value)}
+    >
+      {label}
+    </div>
+  );
+}
+
 export function Chart({
   data
 }: {
   data: {
     date: String;
     ETH: number;
+    timestamp: number;
   }[];
 }) {
+  const [filter, setFilter] = useState(Infinity);
   const [onMobile, setOnMobile] = useState(false);
+  const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    const now = Date.now();
+    const filteredData = data.filter((x) => now - x.timestamp < filter);
+    setFilteredData(filteredData);
+  }, [filter]);
 
   const handleResize = () => {
     setOnMobile(window.innerWidth < 768);
@@ -32,22 +71,16 @@ export function Chart({
   return (
     <div className="mt-4 md:mt-8 rounded mb-16">
       <div className="flex items-center justify-end font-mono text-sm font-bold gap-3 mb-8">
-        <div className="bg-[#2E46C8] rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer">
-          7D
-        </div>
-        <div className="bg-[#242424] rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer">
-          1M
-        </div>
-        <div className="bg-[#242424] rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer">
-          1Y
-        </div>
-        <div className="bg-[#242424] rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer">
-          30D
-        </div>
+        <FilterOption setFilter={setFilter} filter={filter} value={Infinity} label="MAX" />
+        <FilterOption setFilter={setFilter} filter={filter} value={YEAR} label="1Y" />
+        <FilterOption setFilter={setFilter} filter={filter} value={3 * MONTH} label="3M" />
+        <FilterOption setFilter={setFilter} filter={filter} value={MONTH} label="1M" />
+        <FilterOption setFilter={setFilter} filter={filter} value={WEEK} label="7D" />
+        <FilterOption setFilter={setFilter} filter={filter} value={DAY} label="24H" />
       </div>
       <div className="w-full h-full">
-        <ResponsiveContainer width={'100%'} height={330}>
-          <AreaChart data={data}>
+        <ResponsiveContainer width={'100%'} height={330} style={{ padding: 4 }}>
+          <AreaChart data={filteredData}>
             <CartesianGrid vertical={false} horizontal={true} stroke="#ffffff11" />
             <defs>
               <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
@@ -57,16 +90,18 @@ export function Chart({
             </defs>
             <XAxis
               tickLine={false}
-              dataKey="date"
-              className="font-mono text-xs"
               axisLine={false}
+              className="font-mono text-xs"
+              dataKey="date"
               stroke={'#6b7280'}
+              minTickGap={100}
             />
             <YAxis
               tickLine={false}
               className="font-mono text-xs"
               axisLine={false}
               stroke={'#6b7280'}
+              domain={['dataMin', 'dataMax']}
             />
             <Tooltip
               content={(data) => (
@@ -97,6 +132,7 @@ export function Chart({
                 stroke: '#FFFFFF88',
                 strokeWidth: 1
               }}
+              isAnimationActive={false}
             />
           </AreaChart>
         </ResponsiveContainer>

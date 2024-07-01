cd18e98018bbf4dc3ee47a6b9c6d3bc27be52095
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-07-01 11:57:52 -0400

    Aggregated TVL % change works

--- src/components/chart.tsx
+++ src/components/chart.tsx
@@ -14,16 +14,47 @@ const WEEK = 7 * DAY;
 const MONTH = 30 * DAY;
 const YEAR = 12 * MONTH;
 
+const valueToLabel: Record<
+  number,
+  {
+    long: string;
+    short: string;
+  }
+> = {
+  [DAY]: {
+    long: '1 day',
+    short: '24H'
+  },
+  [WEEK]: {
+    long: '7 days',
+    short: '7D'
+  },
+  [MONTH]: {
+    long: '1 month',
+    short: '1M'
+  },
+  [3 * MONTH]: {
+    long: '3 months',
+    short: '3M'
+  },
+  [YEAR]: {
+    long: '1 year',
+    short: '1Y'
+  },
+  [Infinity]: {
+    long: 'All time',
+    short: 'All time'
+  }
+};
+
 function FilterOption({
   filter,
   setFilter,
-  value,
-  label
+  value
 }: {
   filter: number;
   setFilter: (value: number) => void;
   value: number;
-  label: string;
 }) {
   return (
     <div
@@ -34,7 +65,7 @@ function FilterOption({
       }
       onClick={() => setFilter(value)}
     >
-      {label}
+      {valueToLabel[value].short}
     </div>
   );
 }
@@ -65,6 +96,7 @@ export function Chart({
   const [ETHPrice, setETHPrice] = useState(1);
   const [isUSD, setIsUSD] = useState(false);
   const [filteredData, setFilteredData] = useState(data);
+  const [rangeChange, setRangeChange] = useState(0);
 
   useEffect(() => {
     getTokenPrice('ethereum').then((price) => setETHPrice(price));
@@ -83,6 +115,10 @@ export function Chart({
     }
 
     setFilteredData(filteredData);
+    setRangeChange(
+      (100 * (filteredData[filteredData.length - 1].ETH - filteredData[0].ETH)) /
+        filteredData[0].ETH
+    );
   }, [filter, isUSD]);
 
   const handleResize = () => {
@@ -96,97 +132,132 @@ export function Chart({
   }, []);
 
   return (
-    <div className="mt-4 md:mt-8 rounded mb-16">
-      <div className="flex items-center justify-between mb-8">
-        <div className="flex items-center justify-center gap-3 font-mono text-sm font-bold">
-          <div
-            className="rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer"
-            style={{
-              backgroundColor: isUSD ? '#2E46C8' : '#242424'
-            }}
-            onClick={() => setIsUSD(true)}
-          >
-            USD
-          </div>
-          <div
-            className="rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer"
-            style={{
-              backgroundColor: !isUSD ? '#2E46C8' : '#242424'
-            }}
-            onClick={() => setIsUSD(false)}
-          >
-            ETH
+    <div>
+      <div className="mt-16 md:mt-24 flex items-center justify-between gap-8 flex-wrap">
+        <div>
+          <h2 className="text-3xl font-medium mb-2">Liquid Staking</h2>
+          <div className="text-xl opacity-70 max-w-[600px] font-light">
+            Total ETH value of all liquid staking protocol assets.
           </div>
         </div>
-        <div className="flex items-center justify-end font-mono text-sm font-bold gap-3">
-          <FilterOption setFilter={setFilter} filter={filter} value={Infinity} label="MAX" />
-          <FilterOption setFilter={setFilter} filter={filter} value={YEAR} label="1Y" />
-          <FilterOption setFilter={setFilter} filter={filter} value={3 * MONTH} label="3M" />
-          <FilterOption setFilter={setFilter} filter={filter} value={MONTH} label="1M" />
-          <FilterOption setFilter={setFilter} filter={filter} value={WEEK} label="7D" />
-          <FilterOption setFilter={setFilter} filter={filter} value={DAY} label="24H" />
+        <div className="min-w-[120px] text-right">
+          <div className="text-[40px] md:leading-[50px] font-medium">
+            ${Intl.NumberFormat('us').format((ETHPrice * Number(data[data.length - 1].ETH)) / 1e9)}B
+          </div>
+          <div>
+            <div className="flex items-center justify-end gap-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="14"
+                height="12"
+                viewBox="0 0 14 12"
+                fill="none"
+              >
+                <path d="M7 0L13.9282 12H0.0717969L7 0Z" fill="#33FF54"></path>
+              </svg>
+              <div className=" font-mono">
+                <span className="text-green-500">
+                  {Intl.NumberFormat('en-US', {
+                    maximumFractionDigits: 2
+                  }).format(rangeChange)}
+                  %
+                </span>
+                <span className="opacity-60"> /</span> {valueToLabel[filter].long}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="w-full h-full">
-        <ResponsiveContainer width={'100%'} height={330} style={{ padding: 4 }}>
-          <AreaChart data={filteredData}>
-            <CartesianGrid vertical={false} horizontal={true} stroke="#ffffff11" />
-            <defs>
-              <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="5%" stopColor="#3b82f6" stopOpacity={0.2} />
-                <stop offset="95%" stopColor="#3b82f6" stopOpacity={0} />
-              </linearGradient>
-            </defs>
-            <XAxis
-              tickLine={false}
-              axisLine={false}
-              className="font-mono text-xs"
-              dataKey="date"
-              stroke={'#6b7280'}
-              minTickGap={100}
-            />
-            <YAxis
-              tickLine={false}
-              className="font-mono text-xs"
-              axisLine={false}
-              stroke={'#6b7280'}
-              domain={['dataMin', 'dataMax']}
-            />
-            <Tooltip
-              content={(data) => (
-                <div className="bg-[#191919] border border-white border-opacity-10 rounded p-4 text-xs font-mono">
-                  <p className="mb-2">
-                    {data.payload && data.payload.length ? data.payload[0].payload.date : ''}
-                  </p>
-                  <span className="opacity-50 mr-6">
-                    {data.payload && data.payload.length ? data.payload[0].dataKey + ':' : ''}
-                  </span>
-                  {data.payload && data.payload.length
-                    ? Intl.NumberFormat('us')
-                        .format(Math.round(Number(data.payload[0].value)))
-                        .toString()
-                    : ''}
-                </div>
-              )}
-            ></Tooltip>
-            <Area
-              type="monotone"
-              dataKey="ETH"
-              stroke="#3b82f6"
-              strokeWidth={2}
-              fillOpacity={1}
-              fill="url(#colorUv)"
-              activeDot={{
-                fill: '#3b82f6',
-                stroke: '#FFFFFF88',
-                strokeWidth: 1
+      <div className="mt-4 md:mt-8 rounded mb-16">
+        <div className="flex items-center justify-between mb-8">
+          <div className="flex items-center justify-center gap-3 font-mono text-sm font-bold">
+            <div
+              className="rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer"
+              style={{
+                backgroundColor: isUSD ? '#2E46C8' : '#242424'
               }}
-              isAnimationActive={false}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+              onClick={() => setIsUSD(true)}
+            >
+              USD
+            </div>
+            <div
+              className="rounded border border-white border-opacity-10 px-3 py-1 select-none cursor-pointer"
+              style={{
+                backgroundColor: !isUSD ? '#2E46C8' : '#242424'
+              }}
+              onClick={() => setIsUSD(false)}
+            >
+              ETH
+            </div>
+          </div>
+          <div className="flex items-center justify-end font-mono text-sm font-bold gap-3">
+            <FilterOption setFilter={setFilter} filter={filter} value={Infinity} />
+            <FilterOption setFilter={setFilter} filter={filter} value={YEAR} />
+            <FilterOption setFilter={setFilter} filter={filter} value={3 * MONTH} />
+            <FilterOption setFilter={setFilter} filter={filter} value={MONTH} />
+            <FilterOption setFilter={setFilter} filter={filter} value={WEEK} />
+            <FilterOption setFilter={setFilter} filter={filter} value={DAY} />
+          </div>
+        </div>
+        <div className="w-full h-full">
+          <ResponsiveContainer width={'100%'} height={330} style={{ padding: 4 }}>
+            <AreaChart data={filteredData}>
+              <CartesianGrid vertical={false} horizontal={true} stroke="#ffffff11" />
+              <defs>
+                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#3b82f6" stopOpacity={0.2} />
+                  <stop offset="95%" stopColor="#3b82f6" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis
+                tickLine={false}
+                axisLine={false}
+                className="font-mono text-xs"
+                dataKey="date"
+                stroke={'#6b7280'}
+                minTickGap={100}
+              />
+              <YAxis
+                tickLine={false}
+                className="font-mono text-xs"
+                axisLine={false}
+                stroke={'#6b7280'}
+                domain={['dataMin', 'dataMax']}
+              />
+              <Tooltip
+                content={(data) => (
+                  <div className="bg-[#191919] border border-white border-opacity-10 rounded p-4 text-xs font-mono">
+                    <p className="mb-2">
+                      {data.payload && data.payload.length ? data.payload[0].payload.date : ''}
+                    </p>
+                    <span className="opacity-50 mr-6">{isUSD ? 'USD: ' : 'ETH: '}</span>
+                    {data.payload && data.payload.length
+                      ? Intl.NumberFormat('us')
+                          .format(Math.round(Number(data.payload[0].value)))
+                          .toString()
+                      : ''}
+                  </div>
+                )}
+              ></Tooltip>
+              <Area
+                type="monotone"
+                dataKey="ETH"
+                stroke="#3b82f6"
+                strokeWidth={2}
+                fillOpacity={1}
+                fill="url(#colorUv)"
+                activeDot={{
+                  fill: '#3b82f6',
+                  stroke: '#FFFFFF88',
+                  strokeWidth: 1
+                }}
+                isAnimationActive={false}
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </div>
+        <div />
       </div>
-      <div />
     </div>
   );
 }

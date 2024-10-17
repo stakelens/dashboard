8b8712fbb449560781b41e954955c9c52d29e013
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-10-17 16:33:54 -0400

    chore: rename protocols -> projects

--- src/components/project-ranking.tsx
+++ src/components/project-ranking.tsx
@@ -4,15 +4,15 @@ import { ArrowChange } from './arrow-change';
 
 const formatter = new Intl.NumberFormat('en-US', { currency: 'USD' });
 
-export function ProtocolRanking({
-  protocols
+export function ProjectRanking({
+  projects
 }: {
-  protocols: {
+  projects: {
     label: string;
     values: { eth: number }[];
   }[];
 }) {
-  const sortedProtocols = [...protocols].sort((a, b) => {
+  const sortedProjects = [...projects].sort((a, b) => {
     const tvlA = a.values[a.values.length - 1].eth;
     const tvlB = b.values[b.values.length - 1].eth;
     return tvlB - tvlA;
@@ -23,7 +23,7 @@ export function ProtocolRanking({
       <thead className="bg-[#242424] font-mono text-[#949494] border-b border-white border-opacity-10">
         <tr>
           <th className="p-4 text-sm font-medium text-left border-r border-white border-opacity-10">
-            PROTOCOL
+            PROJECT
           </th>
           <th className="p-4 text-sm font-medium text-right border-r border-white border-opacity-10">
             7D CHANGE
@@ -34,12 +34,8 @@ export function ProtocolRanking({
         </tr>
       </thead>
       <tbody className="bg-[#191919]">
-        {sortedProtocols.map((protocol) => (
-          <ProtocolRankingRow
-            label={protocol.label}
-            values={protocol.values}
-            key={protocol.label}
-          />
+        {sortedProjects.map((project) => (
+          <ProjectRankingRow label={project.label} values={project.values} key={project.label} />
         ))}
       </tbody>
     </table>
@@ -50,7 +46,7 @@ function percentChange(a: number, b: number): number {
   return (100 * (a - b)) / b;
 }
 
-function ProtocolRankingRow({ label, values }: { label: string; values: { eth: number }[] }) {
+function ProjectRankingRow({ label, values }: { label: string; values: { eth: number }[] }) {
   const TVL = values[values.length - 1].eth;
   const weekChange = percentChange(values[values.length - 1].eth, values[values.length - 8].eth);
 

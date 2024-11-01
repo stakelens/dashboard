b44989ccf5fc7eefe03a38b16f7ed26f6633eec0
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-10-31 23:51:48 -0400

    refactor: streamline ArrowChange component and simplify change handling logic

--- src/components/arrow-change.tsx
+++ src/components/arrow-change.tsx
@@ -1,63 +1,39 @@
 import type { ReactNode } from 'react';
+import { ArrowUp, ArrowDown, Minus } from 'lucide-react';
 
-export function ArrowChange({ change, children }: { change: number; children: ReactNode }) {
-  const THRESHOLD = 0.005;
+type Change = {
+  color: string;
+  arrow: ReactNode;
+};
 
-  let color = '#949494';
-  let SvgIcon = NeutralSvg;
-
-  if (Math.abs(change) >= THRESHOLD) {
-    if (change > 0) {
-      color = '#22c55e';
-      SvgIcon = UpArrowSvg;
-    } else {
-      color = '#ef4444';
-      SvgIcon = DownArrowSvg;
-    }
+function getChange(change: number, threshold: number = 0.005): Change {
+  if (change > threshold) {
+    return {
+      color: '#22c55e',
+      arrow: <ArrowUp size={16} />
+    };
   }
 
+  if (change < -threshold) {
+    return {
+      color: '#ef4444',
+      arrow: <ArrowDown size={16} />
+    };
+  }
+
+  return {
+    color: '#949494',
+    arrow: <Minus size={16} />
+  };
+}
+
+export function ArrowChange({ change, children }: { change: number; children: ReactNode }) {
+  const { color, arrow } = getChange(change);
+
   return (
-    <div className="flex items-center justify-end gap-2">
-      <SvgIcon color={color} />
-      <div className="font-mono" style={{ color }}>
-        {children}
-      </div>
+    <div className="font-mono flex items-center justify-end gap-2" style={{ color }}>
+      {arrow}
+      {children}
     </div>
-  );
-}
-
-function UpArrowSvg({ color }: { color: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="14"
-      height="12"
-      viewBox="0 0 14 12"
-      style={{ fill: color }}
-    >
-      <path d="M7 0L13.9282 12H0.0717969L7 0Z"></path>
-    </svg>
-  );
-}
-
-function DownArrowSvg({ color }: { color: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="14"
-      height="12"
-      viewBox="0 0 14 12"
-      style={{ fill: color, transform: 'rotate(180deg)' }}
-    >
-      <path d="M7 0L13.9282 12H0.0717969L7 0Z"></path>
-    </svg>
-  );
-}
-
-function NeutralSvg({ color }: { color: string }) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="12" viewBox="0 0 14 12">
-      <rect x="0" y="5" width="14" height="2" fill={color} />
-    </svg>
   );
 }

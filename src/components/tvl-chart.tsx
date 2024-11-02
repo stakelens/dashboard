4c750e8ff32fe0a0a7b70d8fbb8a1f810f6a8ebf
Luis Herasme <luis.alberto.herasme.cuevas@gmail.com>
2024-11-02 18:00:19 -0400

    refactor: move TVLHeader component to a new file for better organization

--- src/components/tvl-chart.tsx
+++ src/components/tvl-chart.tsx
@@ -1,20 +1,14 @@
-import { formatter } from '@/lib/format';
-import { ArrowChange } from './arrow-change';
 import { USDToggele } from './chart/usd-toggle';
 import { useMemo, useRef, useState } from 'react';
 import { Filter, FILTERS } from './chart/filter';
 import { TimeChart } from './chart/time-chart';
 import { useETHPrice } from '@/lib/eth-prices';
 import { DAY } from '@/lib/time-constants';
-import {
-  combineDataPoints,
-  convertChartDenomination,
-  percentChange,
-  type DataPoint
-} from '@/lib/chart-utils';
+import { combineDataPoints, convertChartDenomination, type DataPoint } from '@/lib/chart-utils';
 import { closestDay } from '@/lib/utils';
 import { CopyToClipboard } from './copy-to-clipboard';
 import { LastUpdated } from './last-update';
+import { TVLHeader } from './tvl-header';
 
 function useCombineTVL(
   data: DataPoint[][],
@@ -146,54 +140,6 @@ function LoadingOverlay() {
   return (
     <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
       Loading...
-    </div>
-  );
-}
-
-function TVLHeader({
-  title,
-  logo,
-  description,
-  data,
-  changeRange,
-  isUSD
-}: {
-  title: string;
-  description: string;
-  logo?: string;
-  data: DataPoint[];
-  changeRange: string;
-  isUSD: boolean;
-}) {
-  const rangeChange = percentChange(data);
-  const lastValue = data[data.length - 1];
-  const TVL = lastValue ? formatter.format(lastValue.value) : 0;
-
-  return (
-    <div className="flex flex-wrap items-center justify-between gap-4 md:gap-8">
-      <div>
-        <div className="flex items-center justify-start gap-2 mb-2">
-          {logo && <img src={logo} className="w-8 h-8" />}
-          <h2 className="text-2xl font-medium md:text-3xl">{title}</h2>
-        </div>
-        <div className="text-lg md:text-xl opacity-70 max-w-[600px] font-light">{description}</div>
-      </div>
-      <div className="min-w-[120px] text-right w-full lg:w-auto">
-        <div className="text-[40px] md:leading-[50px] font-medium">
-          {isUSD ? `$ ${TVL}` : `${TVL} ETH`}
-        </div>
-        <ArrowChange change={rangeChange}>
-          <span>
-            {Intl.NumberFormat('en-US', {
-              maximumFractionDigits: 2
-            }).format(rangeChange)}
-            %
-          </span>
-          <span className="text-white">
-            <span className="opacity-60"> /</span> {changeRange}
-          </span>
-        </ArrowChange>
-      </div>
     </div>
   );
 }
---
+++ src/components/tvl-header.tsx
@@ -0,0 +1,51 @@
+import { formatter } from '@/lib/format';
+import { ArrowChange } from './arrow-change';
+import { percentChange, type DataPoint } from '@/lib/chart-utils';
+
+export function TVLHeader({
+  title,
+  logo,
+  description,
+  data,
+  changeRange,
+  isUSD
+}: {
+  title: string;
+  description: string;
+  logo?: string;
+  data: DataPoint[];
+  changeRange: string;
+  isUSD: boolean;
+}) {
+  const rangeChange = percentChange(data);
+  const lastValue = data[data.length - 1];
+  const TVL = lastValue ? formatter.format(lastValue.value) : 0;
+
+  return (
+    <div className="flex flex-wrap items-center justify-between gap-4 md:gap-8">
+      <div>
+        <div className="flex items-center justify-start gap-2 mb-2">
+          {logo && <img src={logo} className="w-8 h-8" />}
+          <h2 className="text-2xl font-medium md:text-3xl">{title}</h2>
+        </div>
+        <div className="text-lg md:text-xl opacity-70 max-w-[600px] font-light">{description}</div>
+      </div>
+      <div className="min-w-[120px] text-right w-full lg:w-auto">
+        <div className="text-[40px] md:leading-[50px] font-medium">
+          {isUSD ? `$ ${TVL}` : `${TVL} ETH`}
+        </div>
+        <ArrowChange change={rangeChange}>
+          <span>
+            {Intl.NumberFormat('en-US', {
+              maximumFractionDigits: 2
+            }).format(rangeChange)}
+            %
+          </span>
+          <span className="text-white">
+            <span className="opacity-60"> /</span> {changeRange}
+          </span>
+        </ArrowChange>
+      </div>
+    </div>
+  );
+}
